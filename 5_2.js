c87038bac24cbf2bca9efc8ddad95d2fce914974
Veogentix <kirdoron@gmail.com>
2023-03-01 12:21:52 +0300

    Initial commit

---
+++ 5_2.js
@@ -0,0 +1,11 @@
+let x = alert('Введите данные');
+if(!a.trim()){
+console.log("Упс, кажется Вы ошиблись");
+} else {
+let x = +a;
+if (typeof x === "number" && !isNaN(x)){
+if(x % 2 === 0) {
+console.log("Это чило чётное")
+} else {
+console.log("Это число нечётное")
+}
---
+++ 5_5.js
@@ -0,0 +1,5 @@
+let arr = [1, 2, "start", [{}]];
+console.log("arr.length", arr.length);
+arr.map((item) => {
+  console.log(item);
+});
